9d4b4e2aabb464c3afca9f83a90a39e219a14bc4
EO <eo.code.wd@gmail.com>
2023-11-23 20:12:29 +0100

    ttl

--- config/session.config.js
+++ config/session.config.js
@@ -27,7 +27,7 @@ module.exports = app => {
         mongoUrl: process.env.MONGODB_URI ,
 
         // ttl => time to live
-        // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+        ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
       })
     })
   );
