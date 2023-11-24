a6c38e89aab8091a3bbd17889f69d8a5d21d7017
EO <eo.code.wd@gmail.com>
2023-11-24 20:07:57 +0100

    Profile Update Route &  CROWD

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,10 +6,40 @@ const dall = require("../classes/VanillaImageGenerator.class.js")
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const User = require("../models/User.model")
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
 
 router.get("/profile", isLoggedIn, (req, res, next) => {
-  res.render("profile/profile", { userInSession: req.session.currentUser });
+
+  const currentUser =  req.session.currentUser
+  const email = currentUser.email;
+  res.render("profile/profile", { userInSession: req.session.currentUser ,email});
+});
+
+router.post('/update',isLoggedIn, async (req, res) => {
+  try {
+    const currentUser = await req.session.currentUser
+    const userId = req.session.currentUser._id; 
+const email = currentUser.email;
+const username = req.body.username;
+const emailNew = req.body.email
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { username, emailNew },
+      { new: true } // To return the updated document
+    );
+
+  
+
+ 
+
+    await user.save();
+
+    res.redirect('/user/profile'); 
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while updating the profile.' });
+  }
 });
 
 
